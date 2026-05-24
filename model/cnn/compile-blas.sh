f37b8f4717e17f4ec733836d98ac8990411c4d4e
tudinhh <anhtu.dinh1202@gmail.com>
2026-05-24 23:45:42 +0200

    change compilation log

--- model/cnn/compile-blas.sh
+++ model/cnn/compile-blas.sh
@@ -8,18 +8,18 @@ CUSTOM_OPT="../../custom-opt/build/bin/custom-opt"
 LIB_PATH="$TORCH_MLIR_DIR/build/lib"
 BUILD="./build"
 
-echo "1. Lowering MLIR to LLVM Dialect..."
+echo "1. Apply MLIR transformations"
 $CUSTOM_OPT build/cnn.mlir \
   -pass-pipeline="builtin.module(linalg-to-bufferization,convert-matmul-to-blas,bufferization-to-llvm)" \
   -o build/cnn_llvm.mlir
 
-echo "2. Translating to LLVM IR..."
+echo "2. Translate MLIR to LLVM IR"
 $TRANSLATE -mlir-to-llvmir build/cnn_llvm.mlir -o build/cnn.ll
 
 echo "3. Compile LLVM IR to .o"
 $LLC -O3 -filetype=obj -relocation-model=pic build/cnn.ll -o build/cnn.o
 
-echo "4. Compiling run.cpp..."
+echo "4. Compile run.cpp"
 $CLANG -O3 run.cpp build/cnn.o -o build/run-blas \
   -L$LIB_PATH \
   -lmlir_c_runner_utils \
